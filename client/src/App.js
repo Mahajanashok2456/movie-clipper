06fbefe721bac46b2626b2284374acc614b6005c
Mahajanashok2456 <ashokroshan78@gmail.com>
2025-05-17 18:48:07 +0530

    Fix API URL for production

--- client/src/App.js
+++ client/src/App.js
@@ -101,7 +101,7 @@ function App() {
     setCancelToken(source);
 
     try {
-      const response = await axios.post('http://localhost:5000/upload', formData, {
+      const response = await axios.post('/upload', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
